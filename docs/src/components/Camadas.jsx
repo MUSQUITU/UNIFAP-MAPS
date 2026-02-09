da3017100a93e2889e967c52fba482835d6e709a
Camila <camilinhaalmeida55@gmail.com>
2026-02-09 20:02:04 -0300

    ⚡️ Change path

---
+++ docs/src/components/Camadas.jsx
@@ -0,0 +1,7 @@
+import { render } from "react-dom";
+import { LayersControl, MapContainer, Marker, Popup, useMap } from "react-leaflet";
+
+export function Camadas(){
+
+
+}
--- docs/src/components/MyMap.jsx
+++ docs/src/components/MyMap.jsx
@@ -1,4 +1,4 @@
-import { MapContainer, TileLayer, GeoJSON } from 'react-leaflet'
+import { MapContainer, TileLayer, GeoJSON, LayersControl, Marker, Popup } from 'react-leaflet'
 
 import './MyMap.css'
 import 'leaflet/dist/leaflet.css'
@@ -8,38 +8,47 @@ import atendimento from '../data/atendimento_ao_publico.json'
 import salaDeAula from '../data/blocos_sala_de_aula.json'
 import graduacao from '../data/cursos_de_graduacao.json'
 import labs from '../data/labs_e_outros.json'
+import area from '../data/unifap-area.json'
+
 
 import { BuscaBox } from './BuscaBox'
+//import { Camadas } from './Camadas'
 
 export function MyMap() {
   const center = [-0.00406,-51.08518]
 
+  const areaStyle = {
+   fillColor: '#7B68EE',
+    fillOpacity: 0,
+    stroke: false
+  }
+
   const areaLazerStyle = {
-    fillColor: '#BA55D3',
-    fillOpacity: 0.5,
+   // fillColor: ,
+    fillOpacity: 0,
     stroke: false
   }
 
   const atendimentoStyle = {
-    fillColor: 'red',
-    fillOpacity: 0.3,
+   fillColor: 'red',
+    fillOpacity: 0,
     stroke: false,
   }
 
   const salaDeAulaStyle = {
-    fillColor: '#7B68EE',
-    fillOpacity: 0.4,
+  //  fillColor: '#7B68EE',
+    fillOpacity: 0,
     stroke: false
   }
 
   const graduacaoStyle = {
-    fillColor: '#8A2BE2',
-    fillOpacity: 0.4,
+  //  fillColor: '#8A2BE2',
+    fillOpacity: 0,
     stroke: false
   }
   const labsStyle = {
-    fillColor: '#78e9cd',
-    fillOpacity: 0.4,
+  //  fillColor: '#78e9cd',
+    fillOpacity: 0,
     stroke: false
   }
 
@@ -67,6 +76,18 @@ export function MyMap() {
 
       <BuscaBox />
 
+      <LayersControl position="topright">
+        <LayersControl.Overlay name="Banheiros">
+        <Marker position= {[-0.0029, -51.0843 ]}>
+         <Popup>
+           banheiro do dcet
+        </Popup>
+        </Marker>
+        </LayersControl.Overlay>
+       </LayersControl>
+
+
+   
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url='https://tile.openstreetmap.org/{z}/{x}/{y}.png'
@@ -75,6 +96,11 @@ export function MyMap() {
     <GeoJSON
       data={areaLazer}
       style={areaLazerStyle}
+      onEachFeature={onEach}
+    />
+    <GeoJSON
+      data={area}
+      style={areaStyle}
       onEachFeature={onEach}
     />
     <GeoJSON
